5786462d464416af673d0279e2ab8750daeb0639
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-11-20 14:28:48 +0300

    add config model

---
+++ pkg/models/app.go
@@ -0,0 +1,13 @@
+// Package models contains models for inventory, config ...
+package models
+
+type Config struct {
+	Service Service `mapstructure:"service"`
+}
+
+type Service struct {
+	Env                  string `mapstructure:"env"`
+	GrpcURL              string `mapstructure:"grpc_url"`
+	CommonServiceGrpcURL string `mapstructure:"common_service_grpc_url"`
+}
+
