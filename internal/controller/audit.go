fee4bf71e8911453c7a717dea0a954ebf8e05c38
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-12-16 16:56:32 +0300

    add Dockerfile.dev

--- internal/controller/audit.go
+++ internal/controller/audit.go
@@ -5,5 +5,5 @@ import "github.com/ahmad-khatib0-org/megacommerce-shared-go/pkg/models"
 // ProcessAudit process the given audit and save it
 // TODO: implement the function
 func (c *Controller) ProcessAudit(ar *models.AuditRecord) {
-	c.log.DebugStruct("the following record should be processed", ar)
+	// c.log.DebugStruct("the following record should be processed", ar)
 }
